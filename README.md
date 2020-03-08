6a0f6dccc0713151b84e12a810ce57788d967d6e
ManoShu <fernando.delphi@hotmail.com>
2020-03-07 23:52:59 -0300

    Basic web server framework

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ARDConnect
 Bridge between Construct 2/3 and an Arduino Uno compatible board.
+
+TODO: How to download files
+
+TODO: How to setup browser (IE, Edge, FF, Chrome)
+
+TODO: How to run web server
+
+TODO: Upload sketch to Arduino, note COM port. also phantom COM devices in case of duplicate COM numbers
+
+TODO: Add plugin to Construct
+
+TODO: Plugin documentation
+
+
---
+++ webserver.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+
+const server = https.createServer({
+  cert: fs.readFileSync('localhost.cert'),
+  key: fs.readFileSync('localhost.key')
+});
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+server.listen(8080);
+console.log("Websocket server running.");
